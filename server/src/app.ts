43f86dd69953fccd81d49fd8744888d7685ebece
ADMohamedd <94984384+Janku13@users.noreply.github.com>
2022-07-08 17:55:52 -0300

    starting server and client

---
+++ server/src/app.ts
@@ -0,0 +1,31 @@
+import express ,{Request,Response} from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import config from 'config'
+import cors from 'cors'
+
+import logger from './utils/logger'
+import {version} from '../package.json'
+
+const port = config.get<number>("port")
+const host = config.get<string>("host")
+const corsOrigin = config.get<string>("corsOrigin")
+
+const app = express()
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: corsOrigin,
+    credentials:true,
+  }
+})
+
+app.get("/", (_:Request,res:Response) =>res.send(`Server is on port ${port} and version ${version}`))
+
+httpServer.listen(port, host, () => {
+  logger.info(`server in listening version ${version}`);
+  logger.info(`server in on http://${host}:${port}`);
+  
+})
