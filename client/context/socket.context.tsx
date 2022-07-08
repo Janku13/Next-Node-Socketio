36acabe2fea5845948292ec0015a6b7214287a17
ADMohamedd <94984384+Janku13@users.noreply.github.com>
2022-07-08 18:12:20 -0300

    connectiong client

--- client/context/socket.context.tsx
+++ client/context/socket.context.tsx
@@ -1,1 +1,13 @@
-import {io}  from "socket.io-client";
+import { createContext, useContext } from "react";
+import { io } from "socket.io-client";
+import { SOCKET_URL } from "../config/defaul"
+
+const socket = io(SOCKET_URL)
+
+const SocketContext = createContext({socket})
+
+function SocketsProvider(props: any) {
+  return <SocketContext.Provider value={{ socket }} {...props} />;
+}
+export const useSockets = () => useContext(SocketContext)
+export default SocketsProvider
